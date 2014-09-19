15a43f3213b011d32b79de5080b684e54fc6eaff
Juho Paaso <jpaaso@juho-pro.local>
2014-09-19 15:04:10 +0300

    Fix: when navbar became fixed, the content jumped up -> added margin

--- public_html/js/navbar.js
+++ public_html/js/navbar.js
@@ -4,9 +4,11 @@ $(document).ready(function() {
         // Make navbar fixed after scrolling down 280 px
         if ($(window).scrollTop() > 280) {
             $('.navbar').addClass('navbar-fixed');
+            $('.content').addClass('below-fixed-navbar');
         }
         if ($(window).scrollTop() < 281) {
             $('.navbar').removeClass('navbar-fixed');
+            $('.content').removeClass('below-fixed-navbar');
         }
     });
 });
