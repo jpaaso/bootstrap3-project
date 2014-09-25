65d8deed32fc561ae42867b9bf46dca468bc75ad
zashenat <zashenat@WL302057.eu.tieto.com>
2014-09-25 10:27:48 +0300

    Dropdown menu

--- public_html/js/main.js
+++ public_html/js/main.js
@@ -1,1 +1,45 @@
 
+var cbpHorizontalMenu = (function() {
+ 
+    var $listItems = $( '#cbp-hrmenu > ul > li' ),
+        $menuItems = $listItems.children( 'a' ),
+        $body = $( 'body' ),
+        current = -1;
+ 
+    function init() {
+        $menuItems.on( 'mouseover', open );
+        $listItems.on( 'mouseover', function( event ) { event.stopPropagation(); } );
+    }
+ 
+    function open( event ) {
+ 
+        if( current !== -1 ) {
+            $listItems.eq( current ).removeClass( 'cbp-hropen' );
+        }
+ 
+        var $item = $( event.currentTarget ).parent( 'li' ),
+            idx = $item.index();
+ 
+        if( current === idx ) {
+            $item.removeClass( 'cbp-hropen' );
+            current = -1;
+        }
+        else {
+            $item.addClass( 'cbp-hropen' );
+            current = idx;
+            //$body.off( 'click' ).on( 'click', close );
+            $('.cbp-hrmenu').on('mouseleave', close);
+        }
+ 
+        return false;
+ 
+    }
+ 
+    function close( event ) {
+        $listItems.eq( current ).removeClass( 'cbp-hropen' ).delay(1800);
+        current = -1;
+    }
+ 
+    return { init : init };
+ 
+})();
